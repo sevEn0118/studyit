2ed16631a8e09453816764766bce9def169c4366
sevEn <liqi0118@126.com>
2017-10-11 23:24:22 +0800

    v1.2 登录基本完成

--- views/static/js/common.js
+++ views/static/js/common.js
@@ -1,8 +1,17 @@
-
-	NProgress.start();
-
-	NProgress.done();
-
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
+$(function () {
+    //从cookie中获取用户信息
+	
+	// 由于不是登录页面，才需要从cookie中获取用户信息
+	if(location.pathname !="/dashboard/login"){
+		var userinfo = $.cookie("userinfo");
+		userinfo = JSON.parse(userinfo);
+    // console.log(userinfo);
+		//"http://static.botue.com/images/avatar/59d78d5cdb0e9.jpg"
+    // tc_name:"前端学院"
+    // 根据获取到的info，创建模版，存放在aside的个人信息中
+		$("#user-info").html(template("profile-tpl",userinfo));
+  }
+	
+	
+	
+})
