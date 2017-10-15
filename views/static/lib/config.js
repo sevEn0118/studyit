94eea9799f11871d559e9a1e81cd94b48db2ddc8
sevEn <liqi0118@126.com>
2017-10-15 22:24:07 +0800

    v3.2 ckeditor完成

--- views/static/lib/config.js
+++ views/static/lib/config.js
@@ -16,7 +16,8 @@ require.config({
     "datepicker":"bootstrap-datepicker/js/bootstrap-datepicker",
     "datepickerCN":"bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min",
     "nprogress":"nprogress/nprogress",
-    "validate":"jquery-validate/jquery-validate"
+    "validate":"jquery-validate/jquery-validate",
+    "ckeditor":"ckeditor/ckeditor"
   },
   shim:{
     "bootstrap":{
@@ -27,6 +28,10 @@ require.config({
    },
     "validate":{
       deps:["jquery"]
+    },
+    "ckeditor":{
+      exports:"CKEDITOR"
+    
     }
   }
 })
